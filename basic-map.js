8af0ab814dbf638198ad677d91567c1f18b0a1fa
Hugh Zhang <hugh_zhang@hotmail.com>
2015-03-10 11:25:54 +0800

    add solutions

---
+++ basic-map.js
@@ -0,0 +1,16 @@
+function repeat(operation, num) {
+    if (num <= 0) return;
+    operation();
+    return repeat(operation, --num);
+}
+
+module.exports = repeat;
+
+/*function repeat(operation, num) {
+    for(var i=0; i<num; i++){
+        operation();
+    }
+}
+
+// Do not remove the line below
+module.exports = repeat*/
